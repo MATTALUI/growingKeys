c234967347eb8bee7e93594e55b2eb391e678f38
Matthew Hummer <biggreyson@gmail.com>
2017-08-21 10:30:46 -0600

    add some test cases

--- growingKeys.js
+++ growingKeys.js
@@ -1,4 +1,6 @@
 function growingKeys(num, str){
+  if (str === '') return new Error('only strings, please');
+  if (typeof str != 'string') return new Error('how dare you.');
 	let obj ={};
 	for ( let i = 1; i<=num;i++){
 		obj[`${str.repeat(i)}`] = true;
@@ -6,5 +8,8 @@ function growingKeys(num, str){
 	return obj;
 }
 
+console.log(growingKeys(2,''));
+console.log(growingKeys(2,[]));
 console.log(growingKeys(2,'d'));
 console.log(growingKeys(4,'sun'));
+// console.log(growingKeys(100,'2'));
